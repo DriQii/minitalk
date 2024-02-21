3c1b9ecb767898221dc24ed2d0ab255f9419d687
Enzo Vella <DriQii>
2024-02-21 21:13:04 +0100

    finish

---
+++ server.c/server.c
@@ -0,0 +1,63 @@
+#include "server.h"
+
+
+void	ft_handler(int sig, siginfo_t *siginfo, void *s);
+
+int	main(void)
+{
+	int					pid;
+	struct sigaction	sa;
+
+	sa.sa_sigaction = ft_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	pid = (int)getpid();
+	ft_printf("PID = %d\n", pid);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while(1);
+	return(0);
+}
+void	ft_add_char(char c, siginfo_t *siginfo)
+{
+	static char	*str;
+	static int	i;
+	char		*newstr;
+	char		*tmp;
+
+	if (!str)
+		str = ft_calloc(sizeof(char), 1);
+	newstr = ft_calloc(sizeof(char), ft_strlen(str) + 2);
+	ft_strcpy(newstr, str);
+	tmp = str;
+	str = newstr;
+	free(tmp);
+	str[i] = c;
+	i++;
+	if (c == 0)
+	{
+		ft_putstr(str);
+		kill(siginfo->si_pid, SIGUSR1);
+	}
+}
+
+void	ft_handler(int sig, siginfo_t *siginfo, void *s)
+{
+	static int i;
+	static int bit;
+
+	(void)s;
+	if(sig == SIGUSR1)
+	{
+		bit = bit | 1 << (7 - i);
+		i++;
+	}
+	else
+		i++;
+	if(i == 8)
+	{
+		ft_add_char(bit, siginfo);
+		i = 0;
+		bit = 0;
+	}
+}
